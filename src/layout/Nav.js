17c2a35ce0e0a2c6a5b0b7acea582c2a98cc81c0
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2020-10-31 12:29:39 +0000

    Map navs

--- src/layout/Nav.js
+++ src/layout/Nav.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
 
+const navs = [
+  {url: '/', name: 'Home'},
+  {url: '/about', name: 'About'},
+  {url: '/contact', name: 'Contact'},
+]
+
 const Nav = () => (
-  <nav className="bg-purple-500 text-white p-4 shadow">
-    <ul className="flex space-x-20 justify-end m-5">
-      <li><NavLink exact activeClassName="border-b-2 border-white" to="/" >Home</NavLink></li>
-      <li><NavLink exact activeClassName="border-b-2 border-white" to="/about" >About</NavLink></li>
-      <li><NavLink exact activeClassName="active" to="/contact" >Contact</NavLink></li>
+  <nav className="bg-purple-700 text-purple-300 shadow">
+    <ul className="flex justify-end">
+      {navs.map((navItem, i) => {
+        return (
+          <li>
+            <NavLink key={i} exact
+                     className="flex space-x-5 p-10 rounded-lg
+                     hover:bg-purple-600
+                     active:bg-purple-800"
+                     activeClassName="text-purple-100"
+                     to={navItem.url}>{navItem.name}
+            </NavLink>
+          </li>)
+      })}
     </ul>
   </nav>
 );
