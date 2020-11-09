653b844e5ddf2a10d35d5fba75d25d522decb22e
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2020-11-09 17:14:52 +0000

    Add Exercises

---
+++ src/pages/Exercises.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Layout } from '../layout'
+
+class Exercises extends React.Component {
+
+  render() {
+    return (
+      <Layout>
+        <div className="p-5">This is the Exercises page.</div>
+      </Layout>
+    )
+  }
+
+}
+
+export default Exercises;
