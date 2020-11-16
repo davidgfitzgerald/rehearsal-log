009653ca792b9fb79e84dfc20fd69b75cd567edf
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2020-11-16 21:22:34 +0000

    Change location of imports and render PracticeIndex component

--- src/pages/practices/Home.js
+++ src/pages/practices/Home.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Layout } from '../layout'
-import { PracticeForm } from "../components/practice_form";
+import { Layout } from '../../layout'
+import { PracticeForm, PracticeIndex } from "../../components/practices";
 
 class Home extends React.Component {
 
   render () {
     return (
       <Layout>
+        <PracticeIndex/>
         <PracticeForm/>
         {/*<Clock/>*/}
         {/*<Toggle/>*/}
