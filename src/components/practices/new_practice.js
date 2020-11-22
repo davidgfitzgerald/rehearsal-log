87422115b93ce26a60181ee451d0108981e9ed85
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2020-11-22 23:29:27 +0000

    Continue Formik refactor

--- src/components/practices/new_practice.js
+++ src/components/practices/new_practice.js
@@ -10,7 +10,7 @@ class PracticeForm extends React.Component {
 
   render() {
     return (
-      <Formik initialValues={{ duration: "", instrument: "drums", bpm: "", exercise_id: null}}
+      <Formik initialValues={{ duration: "", instrument: "drums", bpm: "", exercise_id: ""}}
               validationSchema={Yup.object({
                 duration: Yup.number()
                   .typeError("Must be a number.")
@@ -26,43 +26,48 @@ class PracticeForm extends React.Component {
                   .typeError("Must be a number")
                   .required("Required.")
               })}
-              onSubmit={(values, { setSubmitting }) => {
-                setTimeout(() => {
-                  alert(JSON.stringify(values, null, 2));
-                  setSubmitting(false);
-                }, 400)
-
+              onSubmit={(values) => {
+                alert(JSON.stringify(values, null, 2));
               }}
-      >{formik => (<div className="m-10">
-        <h1 className="ctitle">New Practice</h1>
+      >
+        <div className="m-10">
+          <h1 className="ctitle">New Practice</h1>
 
-        <Form className="cform flex">
-          <div>
-            <label htmlFor="duration">Duration</label>
-            <Field className="cform m-2" name="duration" type="text"/>
-            <ErrorMessage className={"cbutton1 bg-" + ERROR_COLOUR + "-500 text-white"} name="duration"/>
-          </div>
-          <div>
-            <label htmlFor="bpm">BPM</label>
-            <Field className="cform m-2" name="bpm" type="text"/>
-            <ErrorMessage className={"cbutton1 bg-" + ERROR_COLOUR + "-500 text-white"} name="bpm"/>
-          </div>
-          <div>
-            <label htmlFor="instrument">Instrument</label>
-            <Field className="cform m-2" name="instrument" type="text"/>
-            <ErrorMessage className={"cbutton1 bg-" + ERROR_COLOUR + "-500 text-white"} name="instrument"/>
-          </div>
-          <div>
-            <label htmlFor="exercise">Exercise</label>
-            <Field className="cform m-2" name="exercise" type="text"/>
-            <ErrorMessage className={"cbutton1 bg-" + ERROR_COLOUR + "-500 text-white"}  name="exercise"/>
-          </div>
+          <Form className="cform flex">
+            <div>
+              <label htmlFor="duration">Duration</label>
+              <Field className="cform m-2" name="duration" type="text"/>
+              <ErrorMessage className={"cbutton1 bg-" + ERROR_COLOUR + "-500 text-white"} name="duration"/>
+            </div>
+            <div>
+              <label htmlFor="instrument">Instrument</label>
+              <Field as="select" className="cform m-2" name="instrument" type="text">
+                <option value="drums">Drums</option>
+                <option value="guitar">Guitar</option>
+                <ErrorMessage className={"cbutton1 bg-" + ERROR_COLOUR + "-500 text-white"} name="instrument"/>
+              </Field>
+            </div>
+            <div>
+              <label htmlFor="bpm">BPM</label>
+              <Field className="cform m-2" name="bpm" type="text"/>
+              <ErrorMessage className={"cbutton1 bg-" + ERROR_COLOUR + "-500 text-white"} name="bpm"/>
+            </div>
+            <div>
+              <label htmlFor="exercise_id">Exercise</label>
+              <Field as="select" className="cform m-2" name="exercise_id" type="text">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <ErrorMessage className={"cbutton1 bg-" + ERROR_COLOUR + "-500 text-white"}  name="exercise_id"/>
+              </Field>
+            </div>
 
-          <button className={"cbutton1 bg-" + SECONDARY_COLOUR + "-500 text-white"} type="submit">Submit</button>
-        </Form>
-      </div>)}
+            <button className={"cbutton1 bg-" + SECONDARY_COLOUR + "-500 text-white"} type="submit">Submit</button>
+          </Form>
+        </div>
       </Formik>
-    )}
+    )
+  }
 }
 
 export { PracticeForm }
