f71ed8a9d50018cb33a91104c9e9565df4fa156f
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2020-11-23 13:38:30 +0000

    Finish Formik refactor

--- src/components/practices/new_practice.js
+++ src/components/practices/new_practice.js
@@ -14,38 +14,38 @@ class PracticeForm extends React.Component {
   render() {
     const initialValues = { duration: "", instrument: "drums", bpm: "", exercise_id: 1}
     const validationSchema = Yup.object({
-        duration: Yup.number()
-          .typeError("Must be a number.")
-          .min(1, "Must be greater than zero.")
-          .required("Required."),
-        bpm: Yup.number()
-          .typeError("Must be a number.")
-          .min(1, "Must be greater than zero."),
-      })
+      duration: Yup.number()
+        .typeError("Must be a number.")
+        .min(1, "Must be greater than zero.")
+        .required("Required."),
+      bpm: Yup.number()
+        .typeError("Must be a number.")
+        .min(1, "Must be greater than zero."),
+    })
 
     const onSubmit = (values) => {
       setTimeout(() => {
-        fetch(POSTPractice, {
-          method: "POST",
-          body: JSON.stringify(values, null, 2)
-        })
-          .then(res => res.json())
-          .then(
-            (response) => {
-              alert(JSON.stringify(response))
-              this.setState({
-                isLoaded: true,
-                practices: response
+          fetch(POSTPractice, {
+            method: "POST",
+            body: JSON.stringify(values, null, 2)
+          })
+            .then(res => res.json())
+            .then(
+              (response) => {
+                alert(JSON.stringify(response))
+                this.setState({
+                  isLoaded: true,
+                  practices: response
 
-              });
-            },
-            (error) => {
-              this.setState({
-                isLoaded: true,
-                error
-              })
-            }
-          )
+                });
+              },
+              (error) => {
+                this.setState({
+                  isLoaded: true,
+                  error
+                })
+              }
+            )
         }, 400
       )
     }
@@ -54,40 +54,39 @@ class PracticeForm extends React.Component {
       <Formik initialValues={initialValues}
               validationSchema={validationSchema}
               onSubmit={onSubmit}>
-
-        <div className="m-10">
+        <Form>
           <h1 className="ctitle">New Practice</h1>
-
-          <Form className="cform flex">
+          <div className="cform flex">
             <div>
               <label htmlFor="duration">Duration</label>
-              <Field className="cform m-2" name="duration" type="text"/>
-              <ErrorMessage className={"cbutton1 bg-" + ERROR_COLOUR + "-500 text-white"} component="div" name="duration"/>
+              <Field className="cform flex m-2" name="duration" type="text"/>
+              <ErrorMessage className={"cbutton1 bg-" + ERROR_COLOUR + "-500 text-white"} component="div"
+                            name="duration"/>
             </div>
             <div>
               <label htmlFor="instrument">Instrument</label>
-              <Field as="select" className="cform m-2" name="instrument" type="text">
+              <Field as="select" className="cform flex m-2" name="instrument" type="text">
                 <option value="drums">Drums</option>
                 <option value="guitar">Guitar</option>
               </Field>
             </div>
             <div>
               <label htmlFor="bpm">BPM</label>
-              <Field className="cform m-2" name="bpm" type="text"/>
+              <Field className="cform flex m-2" name="bpm" type="text"/>
               <ErrorMessage className={"cbutton1 bg-" + ERROR_COLOUR + "-500 text-white"} component="div" name="bpm"/>
             </div>
             <div>
               <label htmlFor="exercise_id">Exercise</label>
-              <Field as="select" className="cform m-2" name="exercise_id" type="text">
+              <Field as="select" className="cform flex m-2" name="exercise_id" type="text">
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
               </Field>
             </div>
 
-            <button className={"cbutton1 bg-" + SECONDARY_COLOUR + "-500 text-white"} type="submit">Submit</button>
-          </Form>
-        </div>
+            <button className={"cbutton1 bg-" + SECONDARY_COLOUR + "-500 text-white flex"} type="submit">Submit</button>
+          </div>
+        </Form>
       </Formik>
     )
   }
