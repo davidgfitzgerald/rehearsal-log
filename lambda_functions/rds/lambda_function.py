90112e01a70b78c8e30b2152c53739a51be8e551
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2020-11-23 12:51:37 +0000

    Remove lambda_functions

--- lambda_functions/rds/lambda_function.py
+++
@@ -1,8 +0,0 @@
-def lambda_handler(event, context):
-    return {
-        "RDS_ENDPOINT": "rehearsal-log.cdlltxeggpnv.us-east-2.rds.amazonaws.com",
-        "RDS_PORT": 3306,
-        "USERNAME": 'dbaccess',
-        "DATABASE_NAME": "rehearsal_log",
-        "SSL": {'ca': 'rds-combined-ca-bundle.pem'}
-    }
