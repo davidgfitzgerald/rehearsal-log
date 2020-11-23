06d947ebf80db4b3973e21ce7dac9de11f2a7b70
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2020-11-23 17:04:19 +0000

    Remove src/pages/Development.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import Practices from './practices/Practices';
 import About from './About';
-import Development from './Development';
 import Exercises from "./exercises/Exercises";
 
-export { Practices, Exercises, About, Development};
+export { Practices, Exercises, About};
