1dca6e0bee579cbb8afb0dac35311f543018ffda
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2020-11-26 10:18:22 +0000

    Add Auth0 wrapper

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app'
+import { Auth0Provider } from '@auth0/auth0-react'
+
+const AuthDomain = process.env.REACT_APP_AUTH0_DOMAIN
+const AuthClientID = process.env.REACT_APP_AUTH0_CLIENT_ID
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain={AuthDomain}
+    clientId={AuthClientID}
+  redirectUri={window.location.origin}>
     <App/>
-  </React.StrictMode>,
+  </Auth0Provider>,
   document.getElementById('root')
 )
