b008ed63d7ee9896d610250fe24799caf3154707
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2020-11-26 10:26:18 +0000

    Continue auth0 functionality

--- src/pages/exercises/Exercises.js
+++ src/pages/exercises/Exercises.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Layout } from '../../layout'
 import { AllExercises } from "../../components/exercises";
+import LoginButton from "../../components/auth/LoginButton";
 
 class Exercises extends React.Component {
 
   render () {
     return (
       <Layout>
+        <LoginButton/>
         <AllExercises/>
       </Layout>
     )
