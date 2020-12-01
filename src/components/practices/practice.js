7f1e56dc1d5128fd7959c1656a521b26ee13a7e5
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2020-12-01 22:13:01 +0000

    Display only necessary columns

--- src/components/practices/practice.js
+++ src/components/practices/practice.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
+const displayColumns = [
+    {"column": "duration", "display_name": "Duration"},
+    {"column": "bpm", "display_name": "BPM"},
+    {"column": "rating", "display_name": "Rating"},
+    {"column": "name", "display_name": "Name"},
+]
+
 class Practice extends React.Component {
   constructor(props) {
     super(props);
@@ -14,11 +21,15 @@ class Practice extends React.Component {
           <li className=" flex m-2">Practice {data["id"]}:</li>
         </ul>
         <ul className="cpractice">
-          {Object.keys(data).filter(hashKey => hashKey !== "id").map((k) => {
-            if (data[k]) {
-              return <li key={k} className="flex m-2">{k}: {data[k]}</li>
+          {displayColumns.map(({display_name, column}) => {
+            if (data[column]) {
+                if (column === "rating") {
+                    return <li key={column} className="flex m-2">{display_name}: {data[column]}/10</li>
+                } else {
+                    return <li key={column} className="flex m-2">{display_name}: {data[column]}</li>
+                }
             } else {
-              return <li key={k} className="flex m-2">{k}: None</li>
+              return <li key={column} className="flex m-2">{display_name}: None</li>
             }
           })}
         </ul>
