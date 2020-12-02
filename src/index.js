5b3e72df5b1be4c27b4554518aa5064c38b5209e
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2020-12-02 23:40:21 +0000

    Add history to Auth0Provider

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app'
-import { Auth0Provider } from '@auth0/auth0-react'
-
-const AuthDomain = process.env.REACT_APP_AUTH0_DOMAIN
-const AuthClientID = process.env.REACT_APP_AUTH0_CLIENT_ID
+import Auth0ProviderWithHistory from './components/auth/auth0-provider-with-history'
+import { Router } from "react-router";
 
 ReactDOM.render(
-    <Auth0Provider
-        domain={AuthDomain}
-        clientId={AuthClientID}
-        redirectUri={window.location.origin}>
-        <App/>
-    </Auth0Provider>,
-    document.getElementById('root')
+  <Router>
+    <Auth0ProviderWithHistory>
+      <App/>
+    </Auth0ProviderWithHistory>
+  </Router>
+  ,
+  document.getElementById('root')
 )
