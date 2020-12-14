520d4a9d9ee814618503b3a493990f112fe0bdff
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2020-12-14 17:21:18 +0000

    Basic UI test recipe

--- src/test/render.test.js
+++ src/test/render.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render, unmountComponentAtNode} from 'react-dom';
+import { act } from "react-dom/test-utils";
+
 import App from '../app';
 
+let container = null;
+beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+})
+
+
 test('App renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App/>, div);
+    act(()=>{
+        render(<App/>, container);
+    })
 })
 
